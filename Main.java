73f6aaddff063f80a103ccc34847cceb048a6457
celer7841 <celer7841@wrdsb.ca>
2020-11-02 04:57:14 +0000

    finished

--- Main.java
+++ Main.java
@@ -5,6 +5,17 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
+
+  JLabel firstLabel;
+  JLabel secondLabel;
+
+  JTextField fahrenheitText;
+  JTextField celsiusText;
+
+  JButton firstButton;
+  JButton secondButton;
+
+  JPanel mainPanel;
   
 
 
@@ -18,6 +29,45 @@ public class Main implements Runnable, ActionListener{
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
+
+    // initialize the main mainPanel
+    mainPanel = new JPanel();
+    
+    mainPanel.setLayout(null);
+
+
+    firstLabel = new JLabel("Degrees Fahrenheit");
+    firstLabel.setBounds(200, 200, 300, 40);
+    secondLabel = new JLabel("Degrees Celcius");
+    secondLabel.setBounds(200, 250, 300, 40);
+
+    firstButton = new JButton("F -> C");
+    firstButton.setBounds(450, 210, 80, 20);
+    secondButton = new JButton ("C -> F");
+    secondButton.setBounds(450, 260, 80, 20);
+
+    fahrenheitText = new JTextField();
+    fahrenheitText.setBounds(350, 210, 60, 20);
+    celsiusText = new JTextField();
+    celsiusText.setBounds(350, 260, 60, 20);
+
+    firstButton.setActionCommand("F -> C");
+    secondButton.setActionCommand("C -> F");
+
+    firstButton.addActionListener(this);
+    secondButton.addActionListener(this);
+
+    mainPanel.add(firstLabel);
+    mainPanel.add(secondLabel);
+
+    mainPanel.add(firstButton);
+    mainPanel.add(secondButton);
+
+    mainPanel.add(fahrenheitText);
+    mainPanel.add(celsiusText);
+
+    frame.add(mainPanel);
+
  
     
 
@@ -28,7 +78,29 @@ public class Main implements Runnable, ActionListener{
     // get the command from the action
     String command = e.getActionCommand();
 
+    String fahrenheitFirst = fahrenheitText.getText();
+    String celsiusSecond = celsiusText.getText();
+
+    double fahrenheitNum = Integer.parseInt
+    (fahrenheitFirst);
+    double celsiusNum = Integer.parseInt
+    (celsiusSecond);
+
+    if (command.equals("F -> C")){
+
+
+      celsiusNum = (fahrenheitNum - 32 ) * 5/9;
+
+      celsiusText.setText("" + celsiusNum);
+
+    }else if (command.equals("C -> F")){
+
+      fahrenheitNum = (celsiusNum * 9 ) / 5 + 32;
+
+      fahrenheitText.setText("" + fahrenheitNum);
+    }
   }
+
 
   // Main method to start our program
   public static void main(String[] args){
